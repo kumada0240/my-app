03051b46d7442cf0756b4b7449b9605d59d8e321
kumada_kazuki <kumada.work0240@gmail.com>
2021-03-17 22:05:09 +0900

    Create Login LoginScreen and SingUpScreen

--- App.jsx
+++ App.jsx
@@ -5,13 +5,16 @@ import MemoListScreen from './src/screens/MemoListScreen';
 import MemoListDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreatScreen';
-
+import LoginScreen from './src/screens/LoginScreen';
+import SingUpScreen from './src/screens/SingUpScreen';
 
 export default function App() {
   return (
     //<MemoListScreen  />
     //<MemoListDetailScreen  />
     //<MemoEditScreen />
-    <MemoCreateScreen />
-  );
+    //<MemoCreateScreen />
+    //<LoginScreen />
+    <SingUpScreen />
+    );
 }
